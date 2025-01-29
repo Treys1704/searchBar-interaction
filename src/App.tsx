904384dbb3f8ca062fa0e4841863dacbbb5dc7ab
Treys1704 <jacquesmanock17@gmail.com>
2025-01-29 12:49:45 +0400

    configure tailwind v3

--- src/App.tsx
+++ src/App.tsx
@@ -18,14 +18,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="bg-gray-500 border rounded p-3 text-white" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className="text-red-700">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs text-red-700">
         Click on the Vite and React logos to learn more
       </p>
     </>
